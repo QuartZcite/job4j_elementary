707b14bd92222d6c43ba107557d61d7d553e9d0a
QuartZcite <wulfdogster@gmail.com>
2020-06-05 13:58:43 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/ru/job4j/Echo.java

--- src/ru/job4j/Echo.java
+++ src/ru/job4j/Echo.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 public class Echo {
     public static void main(String [] args) {
-        System.out.println("непонятно, то есть если удалить файл и создать такой же то он не выгружается?");}
+        System.out.println("непонятно, то есть если удалить файл и создать такой же то он не выгружается?");
+    }
 
 }
