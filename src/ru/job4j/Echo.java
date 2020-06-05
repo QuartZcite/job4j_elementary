788b36f8111518781f255ab9226e4966a3fc986c
QuartZcite <wulfdogster@gmail.com>
2020-06-05 13:39:37 +0300

    3.1. Загрузка файлов на github [#300870]

--- src/ru/job4j/Echo.java
+++ src/ru/job4j/Echo.java
@@ -1,4 +1,7 @@
 package ru.job4j;
 
 public class Echo {
+    public static void main(String [] args) {
+        System.out.println("непонятно, то есть если удалить файл и создать такой же то он не выгружается?");}
+
 }
