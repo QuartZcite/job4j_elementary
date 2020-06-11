0679abd5a16d44de62745d574d5c18146610dba0
QuartZcite <wulfdogster@gmail.com>
2020-06-11 20:15:20 +0300

    5.5. Простое число [#300899]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,28 @@
+package ru.job4j.loop;
+
+
+public class CheckPrimeNumber {
+
+
+    public static boolean check(int number) {
+
+        boolean natNum = false;
+        for (int index = 2; index <= number; index++) {
+
+
+            if ((number % index) == 0) {
+                if (number == index) {
+                    natNum = true;
+                    break;
+                } else {
+//                    natNum = false;
+                    break;
+                }
+
+            }
+
+
+        }
+    return natNum;
+    }
+}
---
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class CheckPrimeNumberTest {
+    @Test
+    public void when5() {
+        boolean rsl = CheckPrimeNumber.check(5);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when4() {
+        boolean rsl = CheckPrimeNumber.check(4);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void when1() {
+        boolean rsl = CheckPrimeNumber.check(1);
+        assertThat(rsl, is(false));
+    }
+    @Test
+    public void when10() {
+        boolean rsl = CheckPrimeNumber.check(10);
+        assertThat(rsl, is(false));
+    }
+    @Test
+    public void when9() {
+        boolean rsl = CheckPrimeNumber.check(9);
+        assertThat(rsl, is(false));
+    }
+    @Test
+    public void when13() {
+        boolean rsl = CheckPrimeNumber.check(13);
+        assertThat(rsl, is(true));
+    }
+    @Test
+    public void when17() {
+        boolean rsl = CheckPrimeNumber.check(17);
+        assertThat(rsl, is(true));
+    }
+
+
+
+}
