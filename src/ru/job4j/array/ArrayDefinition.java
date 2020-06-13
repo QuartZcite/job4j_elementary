b79bf2d1be0e0e75525297797bae87bf7466ee90
QuartZcite <wulfdogster@gmail.com>
2020-06-13 14:11:11 +0300

    6.0.2. Заполнение массива. [#300831]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,8 +4,16 @@ public class ArrayDefinition {
 
 
     public static void main(String[] args) {
+/*
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+*/
+        String[] names = new String[4];
+        names[0] = "Dmitry";
+        names[1] = "Bella";
+        names[2] = "Nikolay";
+        names[3] = "Daria";
+
     }
 }
