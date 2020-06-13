93ffcb1188f546c4c6928fda04710effa796059c
QuartZcite <wulfdogster@gmail.com>
2020-06-13 14:15:37 +0300

    6.0.2. Заполнение массива. [#300831]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -14,6 +14,9 @@ public class ArrayDefinition {
         names[1] = "Bella";
         names[2] = "Nikolay";
         names[3] = "Daria";
-
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
