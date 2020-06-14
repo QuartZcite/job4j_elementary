37153aaeeba0352b3767fd828f09ad09e680641d
QuartZcite <wulfdogster@gmail.com>
2020-06-14 15:00:38 +0300

    6.3. Массив заполнен true или false [#300820]

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -4,7 +4,7 @@ public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
         boolean ref = data[0];
-        for (int index = 0; index < data.length - 1; index++) {
+        for (int index = 0; index < data.length; index++) {
             if (data[index] != ref) {
                 result = false;
                 break;
