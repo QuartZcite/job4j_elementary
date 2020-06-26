9f008fc9da61701998241b7a0be27eca8a765f3f
QuartZcite <wulfdogster@gmail.com>
2020-06-26 16:19:35 +0300

    1.5. Поля объекта [#300989]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -1,7 +1,33 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = "Еда: " + meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = "Имя: " + nick;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
+        System.out.println("Немного о первом котике.");
+        Cat gav = new Cat();
+        gav.eat("котлета");
+        gav.giveNick("Гав");
+        gav.show();
+        System.out.println("Немного о втором котике.");
+        Cat black = new Cat();
+        black.eat("рыба");
+        black.giveNick("Черный Кот");
+        black.show();
     }
 }
