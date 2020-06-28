adcc8897459dbcf8e28ee2b34bac751fe8f3eb80
QuartZcite <wulfdogster@gmail.com>
2020-06-28 12:01:20 +0300

    2.1. Рефакторинг - Расстояние между точками. [#300984]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -10,35 +10,19 @@ public class PointTest {
 
     @Test
     public void whenDist0() {
-        /* Входные параметры. */
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 1;
-        int y2 = 1;
-        /* ожидаемое значение. Это всегда одна переменная. */
+        Point a = new Point(1, 1);
+        Point b = new Point(1, 1);
         double expected = 0;
-        /* вызов метода, который хотим проверить. */
-        /* Результат вычисления. Это всегда одна переменная. */
-        double out = Point.distance(x1, y1, x2, y2);
-        /* сравнение полученного значения с ожидаемым */
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void whenDist5NegativeInput() {
-        /* Входные параметры. */
-        int x1 = -5;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 0;
-        /* ожидаемое значение. Это всегда одна переменная. */
+        Point a = new Point(-5, 0);
+        Point b = new Point(0, 0);
         double expected = 5;
-        /* вызов метода, который хотим проверить. */
-        /* Результат вычисления. Это всегда одна переменная. */
-        double out = Point.distance(x1, y1, x2, y2);
-        /* сравнение полученного значения с ожидаемым */
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
-
-
 }
