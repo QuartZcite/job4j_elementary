2bfa9a566438bea18bd674a5e0008f64c72257c3
QuartZcite <wulfdogster@gmail.com>
2020-06-29 00:17:58 +0300

    2.2. Рефакторинг - Площадь треугольника. [#300985]

---
+++ test/ru/job4j/oop/PointTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.oop;
+
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class PointTest {
+
+    @Test
+    public void whenDist0() {
+        Point a = new Point(1, 1);
+        Point b = new Point(1, 1);
+        double expected = 0;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenDist5NegativeInput() {
+        Point a = new Point(-5, 0);
+        Point b = new Point(0, 0);
+        double expected = 5;
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
