4339da2c161cf22f21f13de4ff6798c3837b631c
QuartZcite <wulfdogster@gmail.com>
2020-07-01 05:14:19 +0300

    2. Модель данных. [#301006]

---
+++ src/ru/job4j/pojo/Student.java
@@ -0,0 +1,35 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String fio;
+    private String group;
+    private Date intro;
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Date getIntro() {
+        return intro;
+    }
+
+    public void setIntro(Date intro) {
+        this.intro = intro;
+    }
+}
+
+
