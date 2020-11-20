c044a2a86f8f1fe3b51e3de4979611d86e96e0aa
QuartZcite <wulfdogster@gmail.com>
2020-11-20 13:39:24 +0300

    3. Компаратор для строк [#300918] использование Math.min()

--- src/ru/job4j/collection/StringCompare.java
+++ src/ru/job4j/collection/StringCompare.java
@@ -5,14 +5,8 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int rsl = 0, min;
-        if (left.length() > right.length()) {
-            min = right.length();
-        } else if ((left.length() < right.length())) {
-            min = left.length();
-        } else {
-            min = left.length();
-        }
+        int rsl = 0;
+        int min = Math.min(left.length(), right.length());
         for (int i = 0; i < min; i++) {
                 rsl = Character.compare(left.charAt(i), right.charAt(i));
                 if (rsl != 0) {
